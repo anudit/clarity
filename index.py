d1fb8d7cf4358dbfa42b3397041af5e4b8b9b0f7
Anudit Nagar <nagaranudit@gmail.com>
2023-10-14 12:50:28 +0530

    Improvements

--- index.py
+++ index.py
@@ -1,21 +1,26 @@
-import os
-
 import gradio as gr
-from langchain import PromptTemplate
+from dotenv import load_dotenv
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import AsyncHtmlLoader
 from langchain.document_transformers import Html2TextTransformer
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import Chroma
 
 from getlinks import depth_lookup
 
-os.environ["OPENAI_API_KEY"] = "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"
-
-global docs_count
+load_dotenv()
 
 llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
-memory = ConversationBufferMemory(memory_key="chat_history")
+memory = ConversationBufferMemory(k=2, memory_key="chat_history", input_key="chat_history")
+embeddings = OpenAIEmbeddings()
+
+global vectorstore
+vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+
+global docs_count
 
 def build_database(url):
 
@@ -23,21 +28,29 @@ def build_database(url):
 
     loader = AsyncHtmlLoader(links)
     docs = loader.load()
-    html2text = Html2TextTransformer()
+    html2text = Html2TextTransformer(ignore_images=True, ignore_links=True)
     docs = html2text.transform_documents(docs)
+    vectorstore.add_documents(docs)
 
-    return len(docs)
+    return len(vectorstore.get()['documents'])
 
 
 def predict(message, history):
 
-    prompt = PromptTemplate(input_variables=["chat_history", "human_input"], template="""
-    {chat_history}
-    Human: {human_input}
-    Chatbot:""")
+    context = vectorstore.similarity_search_with_relevance_scores(message)
+
+    prompt = PromptTemplate(
+        input_variables=["chat_history", "human_input", "context"], 
+        template="""
+        CONTEXT:{context}
+
+        {chat_history}
+        Human: {human_input}
+        Chatbot:"""
+    )
 
     llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True, memory=memory)
-    output = llm_chain.predict(human_input=message)
+    output = llm_chain.run(human_input=message, context=context[:2])
 
     return output
 
@@ -52,13 +65,13 @@ with gr.Blocks() as demo:
 
     with gr.Row():
 
-        docs_count = gr.Label(value=0, label="Docs in Databse")
+        docs_count = gr.Label(value=len(vectorstore.get()['documents']), label="Docs in Databse")
 
         with gr.Group():
 
-            input_url = gr.Textbox(label="URL", lines=1, scale=4)
+            input_url = gr.Textbox(label="URL", lines=1)
             
-            btn = gr.Button(value="Import Website", scale=1)
+            btn = gr.Button(value="Import Website")
             btn.click(build_database, inputs=[input_url], outputs=[docs_count])        
 
 
